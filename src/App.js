1703d9abc39b3cf28e7aa514e1feecad7e438f65
mandeepsingh2007 <singh.mandeeep2007@gmail.com>
2024-10-22 13:58:17 +0530

    add registration form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import Dashboard from './components/Dashboard';
-
+import Banner from './components/Banner';
 function App() {
   return (
     <ChakraProvider>
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginForm />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/banner" element={<Banner />} />
         </Routes>
       </Router>
     </ChakraProvider>
