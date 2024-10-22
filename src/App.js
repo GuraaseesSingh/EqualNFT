e14f33ef4656d20e7170f7c224e0e736aba71b14
Guraasees Singh <guraasees04@gmail.com>
2024-10-22 15:05:17 +0530

    /

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import Dashboard from './components/Dashboard';
+import Contributions from './components/Contributions';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginForm />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/contributions" element={<Contributions />} /> {/* New Contributions Page */}
         </Routes>
       </Router>
     </ChakraProvider>
