81fef87eda06ef83c422f2ca8f7fb73ce38d1468
mandeepsingh2007 <singh.mandeeep2007@gmail.com>
2024-10-22 13:59:47 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import Dashboard from './components/Dashboard';
 import Banner from './components/Banner';
+import RegisterForm from './components/RegisterForm';
 function App() {
   return (
     <ChakraProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginForm />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/banner" element={<Banner />} />
         </Routes>
