57fef5795c975d50472c561f882154ac323496f2
Guraasees Singh <guraasees04@gmail.com>
2024-10-23 19:51:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
           <Route path="/" element={<HomePage />} />
 
           {/**route for register form */}
-          <Route path="/" element={<RegisterForm />} />
           <Route path="/register" element={<RegisterForm />} />
 
           {/* Route for LoginForm */}
